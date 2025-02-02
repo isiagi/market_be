3df548b51814aa6bed9926aede238c3ca6974d89
isiagi <isiagigeofrey0@gmail.com>
2025-02-02 19:12:31 +0300

    chore

--- category/views.py
+++ category/views.py
@@ -10,6 +10,20 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     lookup_field = 'slug'
 
+    # Get only the top-level categories
+    @action(detail=False, methods=['get'])
+    def top_categories(self, request):
+        top_categories = Category.objects.filter(parent=None)
+        serializer = CategorySerializer(top_categories, many=True)
+        return Response(serializer.data)
+    
+    # Get all subcategories from all categories
+    @action(detail=False, methods=['get'])
+    def all_subcategories(self, request):
+        subcategories = Category.objects.filter(parent__isnull=False)
+        serializer = CategorySerializer(subcategories, many=True)
+        return Response(serializer.data)
+
     @action(detail=True, methods=['get'])
     def subcategories(self, request, slug=None):
         category = self.get_object()
