b3c6a9e4f39e25cc56487252397966f54faffa52
isiagi <isiagigeofrey0@gmail.com>
2025-02-03 10:47:48 +0300

    chore

--- order/serializers.py
+++ order/serializers.py
@@ -5,10 +5,13 @@ class OrderSerializer(serializers.ModelSerializer):
     seller = serializers.SerializerMethodField()
     product_name = serializers.SerializerMethodField()
     product_price = serializers.SerializerMethodField()
+    seller_name = serializers.CharField(source='seller.business_name', read_only=True)
+    seller_contact_phone = serializers.CharField(source='seller.contact_phone', read_only=True)
+    seller_contact_email = serializers.CharField(source='seller.contact_email', read_only=True)
     
     class Meta:
         model = Order
-        fields = ['id', 'buyer', 'seller', 'spare_part', 'product_name', 'fullName', 'email', 'phone', 'product_price',
+        fields = ['id', 'buyer', 'seller', 'spare_part', 'product_name', 'seller_name','seller_contact_phone', 'seller_contact_email', 'fullName', 'email', 'phone', 'product_price',
                  'quantity', 'total_price', 'status', 'created_at', 'updated_at']
         read_only_fields = ['buyer', 'seller', 'product_name', 'product_price']
 
