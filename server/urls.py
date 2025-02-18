32458d939512a99e94cc4a811232672e7da84208
isiagi <isiagigeofrey0@gmail.com>
2025-02-18 06:33:16 +0300

    chore:  add django images

--- server/urls.py
+++ server/urls.py
@@ -29,3 +29,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    from django.views.static import serve
+
+    urlpatterns += [
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
